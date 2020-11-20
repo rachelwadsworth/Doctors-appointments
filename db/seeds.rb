46eecba09393d825ff4c45d99b1393d1efdfbde9
rachelwadsworth-smith <rachel.wadsworthsmith@gmail.com>
2020-11-19 17:09:03 -0700

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#patients first_name last_name
+
+users = []
+doctors =[]
+roles = ["eye", "heart", "butt"]
+5.times do |i|
+users<< User.create(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name )  
+end
+
+#create Doctors Last name
+3.times do |i|
+  doctors<< Doctor.create(name: Faker::Creature::Dog.name )
+end
+  #go to each users and add 10 appointments
+  doctors.each do |doctor|
+    9.times do
+  doctor.appointments.create(role: roles.sample, user_id: users.sample.id)
+    end 
+  end
+
+  puts "seeded"
+ 
